c51d34bc90ea8488f477102db1dc22a94188bfc0
Simon Weis <laguna_1989@gmx.net>
2021-08-09 07:50:53 +0200

    Add plotter to tests

--- tests/euler_exp_test.cpp
+++ tests/euler_exp_test.cpp
@@ -2,6 +2,7 @@
 #include "euler.hpp"
 #include <fstream>
 #include <gtest/gtest.h>
+#include <plotter.hpp>
 
 TEST(EulerExponential, Exponential)
 {
@@ -13,7 +14,17 @@ TEST(EulerExponential, Exponential)
 
     std::ofstream file { "euler_exp.txt" };
     for (auto const& kvp : values) {
-        std::cout << kvp.first << " " << exp(kvp.first) << " " << kvp.second << std::endl;
         file << kvp.first << " " << exp(kvp.first) << " " << kvp.second << std::endl;
     }
+
+    std::map<double, std::valarray<double>> data;
+    for (auto const& kvp : values) {
+        data[kvp.first] = std::valarray<double> { kvp.second };
+    }
+
+    Plotter plotter;
+    plotter.set_xrange(-0.5, 3.5);
+    plotter.set_yrange(0, 22.5);
+    plotter.add_data(data);
+    plotter.write_png("exp.png", 800, 800);
 }
