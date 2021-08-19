97a884dc4653d3164439b59b71a680fd42852274
Simon Weis <laguna.ws@gmail.com>
2021-08-19 09:25:22 +0200

    Fix cpp solution

--- cpp/src/euler.hpp
+++ cpp/src/euler.hpp
@@ -12,8 +12,6 @@ T explicit_euler_integrate_one_step(std::function<T(T)> func, T y0, double delta
 {
     return func(y0) * delta + y0;
 
-    // fake implementataion
-    //return y0;
 }
 
 template <typename T>
@@ -21,9 +19,7 @@ std::map<double, T> explicit_euler_integrate(
     std::function<T(T)> func, T y0, double delta, double t_start, double t_end)
 {
     std::map<double, T> values;
-    values[t_start] = y0;
-
-    // to be implemented by user
+	
     double t = t_start;
     T y = y0;
     while (t <= t_end)
@@ -33,12 +29,6 @@ std::map<double, T> explicit_euler_integrate(
         t += delta;
     }
     return values;
-
-    // fake implementation
-//    std::map<double, T> values;
-//    values[42.0] = y0;
-//    return values;
 }
-
 
 #endif
